2d617b3cdba828aefbc1fbbe93e1a6000b743943
unknown <jaroldenderez69@gmail.com>
2025-09-12 17:39:00 +0800

    vite environment change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // React
+  baseURL: import.meta.env.VITE_API_URL, // React
   // baseURL: process.env.NEXT_PUBLIC_API_URL, // Next.js
 });
 
